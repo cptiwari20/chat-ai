b1478100bc5d53f7bfc5c307c3b9810af6d8a97b
Chandra Prakash <cptiwari20@gmail.com>
2024-02-19 08:44:37 +0530

    Added code splitter

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,18 +1,22 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
-    """
-    Generate and store embeddings for the given pdf
+    loader = PyPDFLoader(pdf_path)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
 
-    1. Extract text from the specified PDF.
-    2. Divide the extracted text into manageable chunks.
-    3. Generate an embedding for each chunk.
-    4. Persist the generated embeddings.
+    docs = loader.load_and_split(text_splitter)
 
-    :param pdf_id: The unique identifier for the PDF.
-    :param pdf_path: The file path to the PDF.
+    print(docs)
 
-    Example Usage:
 
-    create_embeddings_for_pdf('123456', '/path/to/pdf')
-    """
 
+
+
+
+    # embedding = OpenAIEmbeddings()
     pass
