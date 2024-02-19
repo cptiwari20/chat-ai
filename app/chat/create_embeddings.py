110a1f257de015c63876ef575c6b7680de433c7d
Chandra Prakash <cptiwari20@gmail.com>
2024-02-19 08:54:32 +0530

    fixed the file upload error

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,8 +1,11 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+import sys
+import io
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
     loader = PyPDFLoader(pdf_path)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
