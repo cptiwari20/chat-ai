3d7dd4d4f0159bfa445667fb0193478c91c8a684
Chandra Prakash <cptiwari20@gmail.com>
2024-02-21 09:13:20 +0530

    Added converstaion Retireval Chain for the achat

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,7 +1,24 @@
+from langchain.chains import ConversationalRetrievalChain
 from app.chat.models import ChatArgs
+from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.memory.sql_memory import build_memory
+from app.chat.llms.chat_openai import build_llm
+
 
 
 def build_chat(chat_args: ChatArgs):
+    retriever = build_retriever(chat_args=chat_args)
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
+
+    return ConversationalRetrievalChain(
+        llm=llm,
+        memory=memory,
+        retriever=retriever
+    )
+
+
+
     """
     :param chat_args: ChatArgs object containing
         conversation_id, pdf_id, metadata, and streaming flag.
--- app/chat/vector_stores/pinecone.py
+++ app/chat/vector_stores/pinecone.py
@@ -11,3 +11,8 @@ pinecone.init(
 vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
+
+def build_retriever(chat_args):
+    return vector_store.as_retriever(
+        search_kwargs={"filter": {"pdf_id": chat_args.pdf_id}}
+    )
