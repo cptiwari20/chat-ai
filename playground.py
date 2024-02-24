51b2bf1a21508ad88558ec201ef852cc63faf106
Chandra Prakash <cptiwari20@gmail.com>
2024-02-24 09:03:37 +0530

    Added streaming with chains

--- playground.py
+++ playground.py
@@ -1,17 +1,33 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+class HandleStreaming(BaseCallbackHandler):
+    def on_llm_new_token(self, token, **kwargs):
+        print(token)
+
+chat = ChatOpenAI(
+    streaming=True, #this allows OpenAI to send data to Langchain in streaming format.
+    callbacks=[
+        HandleStreaming() #this allows Langchain to handle streamed data handle.
+        ]
+    )
 
 prompt = ChatPromptTemplate.from_messages([
     ('human', "{content}")
 ])
 
-messages = prompt.format_messages(content="Tell me a joke")
+chain = LLMChain(llm=chat, prompt=prompt)
 
-for output in chat.stream(messages):
-    print(output.content)
+
+for chunk in chain.stream(input={"content":"tell me one fantastic joke"}):
+    print(chunk)
+# messages = prompt.format_messages(content="Tell me a joke")
+
+# for output in chat.stream(messages):
+#     print(output.content)
 
