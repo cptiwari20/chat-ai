0424624810cdb2bcfbf4978ff2385a4a389112da
Chandra Prakash <cptiwari20@gmail.com>
2024-02-25 09:38:38 +0530

    Added queue to the steaming chain

--- playground.py
+++ playground.py
@@ -3,11 +3,15 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
+from queue import Queue
 
 load_dotenv()
 
+queue = Queue()
+
 class HandleStreaming(BaseCallbackHandler):
     def on_llm_new_token(self, token, **kwargs):
+        queue.put(token)
         pass
 
 chat = ChatOpenAI(
@@ -24,7 +28,9 @@ prompt = ChatPromptTemplate.from_messages([
 class StreamingChain(LLMChain):
     def stream(self, input):
         self(input)
-        yield "Hi there"
+        while True:
+            token = queue.get()
+            yield token
 
 chain = StreamingChain(llm=chat, prompt=prompt)
 
