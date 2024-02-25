efb3392d798fa922862498745c3fb396832de346
Chandra Prakash <cptiwari20@gmail.com>
2024-02-25 10:52:23 +0530

    Handle the streaming memory

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -1,14 +1,26 @@
+from typing import Any, Dict, List
+from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain_core.messages import BaseMessage
 
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue) -> None:
         self.queue = queue
+        self.streaming_chat_ids = set()
+    
+    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], *, run_id: UUID, parent_run_id: UUID | None = None, tags: List[str] | None = None, metadata: Dict[str, Any] | None = None, **kwargs: Any) -> Any:
+        if serialized["kwargs"]["streaming"] is True:
+            self.streaming_chat_ids.add(run_id)
+            print("Streaming chain Id, ", run_id)
+        
 
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
 
-    def on_llm_end(self, response, **kwargs):
-        self.queue.put(None)
+    def on_llm_end(self, response, run_id, **kwargs):
+        if run_id in self.streaming_chat_ids:
+            self.queue.put(None)
+            self.streaming_chat_ids.remove(run_id)
 
     def on_llm_error(self, error):
         self.queue.put(None)
--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.memory.sql_memory import build_memory
@@ -9,11 +10,13 @@ from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args=chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
     # base_combine_doc = BaseCombineDocumentsChain()
 
     return StreamingConversationalRetrievalChain.from_llm(
+        condense_question_llm=condense_question_llm, #second chain that will not stream
         # combine_docs_chain=base_combine_doc,
         llm=llm,
         memory=memory,
