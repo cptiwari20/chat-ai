d73d147b14f3f99d9688eb3a9f7ac01103c24b82
Chandra Prakash <cptiwari20@gmail.com>
2024-02-26 09:02:39 +0530

    Get and set the retrievel to the conversation

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,14 +1,42 @@
+from random import random
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
-from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.vector_stores import retrieval_map
 from app.chat.memory.sql_memory import build_memory
 from app.chat.llms.chat_openai import build_llm
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from web.api import (
+    get_conversation_components,
+    set_conversation_components
+)
 
 
 
 def build_chat(chat_args: ChatArgs):
-    retriever = build_retriever(chat_args=chat_args)
+    
+    conversation = get_conversation_components(
+        conversation_id=chat_args.conversation_id
+    )
+
+    previous_retriever = conversation["retriever"]
+    retriever = None
+    if previous_retriever:
+        build_retriever = retrieval_map[previous_retriever]
+        retriever = build_retriever(chat_args)
+    else:
+        retriever_name = random(list(retrieval_map.keys()))
+        build_retriever = retrieval_map[retriever_name]
+        retriever = build_retriever(chat_args)
+        set_conversation_components(
+            conversation_id=chat_args.conversation_id,
+            retriever=retriever_name,
+            llm="",
+            memory=""
+        )
+
+
+
+
     llm = build_llm(chat_args)
     condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
